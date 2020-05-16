a237438b1cbf945add0b94b08cfef3af3065ca5c
Jilles Oldenbeuving <ojilles@gmail.com>
2020-05-16 23:44:40 +0200

    Create drive-github-backups.sh

---
+++ drive-github-backups.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+REPOS="ojilles/data_hacks ojilles/dotfiles ojilles/hadoop-test ojilles/heatmaps ojilles/iPhoneTracker ojilles/jilles.net ojilles/Nordea-importer-for-Yunoo";
+
+for repo in $REPOS;
+do
+   ~/bin/backup-github.com.sh $repo;
+done
